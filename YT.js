1b43d251b08576972184b921430c293c847da9ed
Mark1237200 <0915kimjw@gmail.com>
2022-03-14 18:50:35 +0900

    delete

--- YT.js
+++
@@ -1,9 +0,0 @@
-const moreBtn = document.querySelector('.info .metadata .titleAndButton .moreBtn');
-const title = document.querySelector('.info .metadata .titleAndButton .title .clamp');
-
-moreBtn.addEventListener('click', () => {
-    moreBtn.classList.toggle('clicked');
-    title.classList.toggle('clamp');
-})
-
-console.log;

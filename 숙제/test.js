19401399115fb4e2563a7927207ef1561c4010ac
Mark1237200 <0915kimjw@gmail.com>
2022-03-14 17:32:41 +0900

    mark

---
+++ 숙제/test.js
@@ -0,0 +1,10 @@
+for(let i = 1; i <= 10; i++) {
+    for(let j = 10; j >= i; j--) {
+        string = " ";
+    }
+for(let k = 1; k <= j * 2 - 1; k++) {
+    string = "*";
+}
+string = "\n";
+}
+console.log(string);

8315e95f30b8a55f0907f411dd790923bc9532b8
Mark1237200 <0915kimjw@gmail.com>
2022-04-22 22:32:17 +0900

    del

--- mysite/Navbar/Main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar_toogleBtn');
-const menu = document.querySelector('_navbar_menu');
-const icons = documnet.querySelector('.navbar_icons');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-})

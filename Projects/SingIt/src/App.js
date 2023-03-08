b23af0225b743a00cdc6f2a4c9874d13636a52e4
Mark1237200 <0915kimjw@gmail.com>
2023-03-08 23:23:50 +0900

    feat: Acoustic connect error

---
+++ Projects/SingIt/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Acoustic } from "./utils/Acoustic.js";
+
+function App() {
+  // const result = Acoustic();
+  return (
+    <a2>
+      정확도는 <Acoustic /> 입니다.
+    </a2>
+  );
+}
+
+export default App;
--- Projects/SingIt/webpack.config.js
+++ Projects/SingIt/webpack.config.js
@@ -1,10 +1,37 @@
-import { resolve } from "path";
+// webpack.config.js
 
-export const mode = "development";
-export const entry = {
-  main: "./src/app.js",
-};
-export const output = {
-  path: resolve("./dist"),
-  filename: "[name].js",
+const path = require("path");
+
+module.exports = {
+  name: "React-webpack-setting", // 웹팩 설정 이름
+  mode: "development", //실서비스 : Production
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    // 합쳐질 파일 요소들 입력
+    app: ["./src/index.js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_module/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [],
+        },
+      },
+    ],
+  },
+  output: {
+    // 최종적으로 만들어질 js
+    path: path.join(__dirname, "/dist"), //빌드 위치
+    filename: "app.js", //웹팩 빌드 후 최종적으로 만들어질 파일
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
 };
